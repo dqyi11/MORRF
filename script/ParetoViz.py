355a89b2578c1854dce118b037f2c434250ee512
walter <daqing.yi@byu.edu>
2016-03-18 13:38:28 -0600

    add viz file

---
+++ script/ParetoViz.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import numpy as np  
+import sys         
+
+def viz(score):
+    fig = plt.figure()     
+    ax = fig.add_subplot(111)
+    pl = ax.plot(scores[:,0], scores[:,1],'bs',label="Score of Path")
+        
+    ul = ax.plot(scores[0,0], scores[1,1], 'ro', label="Utopia Reference Vector")    
+    ax.set_xlabel("Objective 1", fontsize='x-large')
+    ax.set_ylabel("Objective 2", fontsize='x-large')
+    ax.legend( numpoints=1, loc='upper right', shadow=True, fontsize='x-large')    
+    plt.show()
+
+
+def load(filename):
+ 
+    scores = []
+    with open(filename, 'r') as f:
+        for line in f:
+            line = line.replace('\n','')      
+            ss = line.split('\t')
+            print ss
+            scores.append([ float(ss[0]), float(ss[1]) ])
+    return np.array(scores);
+
+if __name__ == '__main__':
+
+    filename = sys.argv[1]
+
+    scores = load(filename)
+    viz(scores)
